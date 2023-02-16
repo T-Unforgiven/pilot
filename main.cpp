065740f99fac51d71d90395981d36e0b3faa1aab
DIA <mrbrused20@gmail.com>
2023-02-17 00:28:11 +0300

    Max&min functions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "bitset"
 
+
+
 int bitcounter(int a){
     int count = 0;
     std::bitset<32> b(a);
@@ -11,9 +13,32 @@ int bitcounter(int a){
     return count;
 }
 
+double maximum(double a[], int n){
+    double currentmax = a[0];
+    for(int i = 0; i < n; i++)
+        if(currentmax < a[i])
+            currentmax = a[i];
+
+    return currentmax;
+}
+
+double minimum(double a[], int n){
+    double currentmin = a[0];
+    for(int i = 0; i < n; i++)
+        if(currentmin > a[i])
+            currentmin = a[i];
+
+    return currentmin;
+}
+
 int main() {
     int a;
     std::cin >> a;
     std::cout << bitcounter(a);
+    auto* Array = new double [a];
+    for(int i = 0; i < a; i++)
+        std::cin >> Array[i];
+    std::cout << maximum(Array, a);
+    std::cout << minimum(Array, a);
     return 0;
 }
