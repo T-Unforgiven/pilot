bfbe72292b33648de3a8d63294949bec8c4915c4
DIA <mrbrused20@gmail.com>
2023-02-17 00:01:45 +0300

    How many bits == 1?

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "bitset"
+
+int bitcounter(int a){
+    int count = 0;
+    std::bitset<32> b(a);
+    for(int i = 0; i < 32; i++){
+        if(b[i] == 1)
+            count++;
+    }
+    return count;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int a;
+    std::cin >> a;
+    std::cout << bitcounter(a);
     return 0;
 }
